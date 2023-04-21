ef6c1cfd38338df633697256e524bb173aba5f24
austinmdx <austinmaddox@maddoxbox.com>
2023-04-21 11:35:28 -0400

    added console.log

--- supabaseServer.ts
+++ supabaseServer.ts
@@ -4,4 +4,7 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.SUPABASE_SERVICE_KEY!;
 
+console.log('supabaseUrl:', supabaseUrl);
+console.log('supabaseKey:', supabaseKey);
+
 export const supabaseServer = createClient(supabaseUrl, supabaseKey);
